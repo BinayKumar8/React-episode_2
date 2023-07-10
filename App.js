4038770d2c5c3c5678c56f618971ad49b4cc3abf
Binay Kumar Das <85559080+binaykumardas@users.noreply.github.com>
2023-07-10 21:20:55 +0530

    update on episode 3
    update on episode 3

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement("div", {
-    id: "parent",
-    name: "Binay"
-}, 
- [React.createElement("div", { id: "child" }, 
- [React.createElement("h1", {}, "This is namaste react ep-2 ❤"),React.createElement("h2", {}, "I am a h2 tag")]),
- React.createElement("div", { id: "child2" }, 
- [React.createElement("h1", {}, "I am a h1 tag"),React.createElement("h2", {}, "I am a h2 tag")])]
- );
+// JSX
+const jsxHeading = <h1>Namaste React using JSX 🚀</h1>; // This is JSX.
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
