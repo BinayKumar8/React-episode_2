e56841401fec1d4c393fcfb8cd7210722063f35c
Binay Kumar Das <85559080+binaykumardas@users.noreply.github.com>
2023-08-08 19:58:00 +0530

    progress
    progress

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,31 @@
 import ResturantCard from "./ResturantCard";
-import resList from "../commonUtil/mockData";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import Shimer from "./Shimer";
 
 
 const Body = () => {
   // React Hook
-  const [listOfResturants,setListOfResturants] = useState(resList); 
+  const [listOfResturants,setListOfResturants] = useState([]); 
 
-    return (
+  useEffect(() => {
+    fetchDataFromAPI();
+  }, []);
+
+  const fetchDataFromAPI = async () => {
+    const API = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.2841955&lng=85.8204646&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+    const data = await fetch(API);
+    const jsonResponse = await data.json();
+    //console.log(jsonResponse.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
+    setListOfResturants(jsonResponse?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  };
+
+  // Conditional Rendering
+  // if(listOfResturants.length === 0) {
+  //    return <Shimer />
+  // }
+
+
+    return listOfResturants.length === 0 ? <Shimer /> : (
         <div className="body">
             <div className="filter">
               <button className="filter-btn"
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import {LOGO_URL} from "../commonUtil/constant";
+import { useState } from "react";
 
 const Header = () => {
+
+    const [reactLogin,setReactLogin] = useState("Login");
+
     return (
         <div className="header">
             <div className="log-container">
@@ -12,6 +16,9 @@ const Header = () => {
                         <li className="list">Contact Us</li>
                         <li className="list">About Us</li>
                         <li className="list">Cart</li>
+                        <button className="login-btn" onClick={() => {
+                            reactLogin === "Login" ? setReactLogin("Logout") : setReactLogin("Login");
+                        }}>{reactLogin}</button>
                     </ul>
             </div>
         </div>
